f7083cb592b99bccaeb846ec1617062ff070985b
omar.bouteldja <omar.bouteld@gmail.com>
2023-03-31 14:25:14 +0200

    A jour JS

---
+++ Exercice 1/TP.js
@@ -0,0 +1,10 @@
+btnHide = document.getElementById('cacher');
+
+btnHide.addEventListener('click', function(){
+
+    var div = document.getElementById('poulet');
+    div.style.display = 'none';
+});
+
+
+    
